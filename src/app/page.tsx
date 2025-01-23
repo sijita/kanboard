276875f5954f45e971495da2655c072b83556657
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-22 21:09:06 -0500

    feat: add edit task modal to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AddTaskButton from '@/components/add-task-button';
 import ColumnsRows from '@/components/columns-rows';
-import TaskModal from '@/components/task-modal';
+import AddTaskModal from '@/components/modals/add-task-modal';
+import EditTaskModal from '@/components/modals/edit-task-modal';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 p-8">
-      <TaskModal />
+      <AddTaskModal />
+      <EditTaskModal />
       <div className="max-w-7xl mx-auto space-y-5">
         <div className="flex justify-between items-center flex-wrap gap-5">
           <h1 className="text-3xl font-bold text-gray-900">Kanboard</h1>
