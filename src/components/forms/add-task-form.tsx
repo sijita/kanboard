66c0d71ed97e6c43befdeb7ca9f4ccec11188564
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-22 22:50:14 -0500

    styles: delete cancel button and increase the add button width

--- src/components/forms/add-task-form.tsx
+++ src/components/forms/add-task-form.tsx
@@ -5,7 +5,7 @@ import Button from '@/components/ui/button';
 import useAddTaskForm from '@/hooks/use-add-task-form';
 
 export default function AddTaskForm() {
-  const { users, submitAction, closeModal } = useAddTaskForm();
+  const { users, submitAction } = useAddTaskForm();
 
   return (
     <form action={submitAction}>
@@ -27,15 +27,9 @@ export default function AddTaskForm() {
       </div>
       <div className="mt-5 flex justify-end gap-3">
         <Button
-          text="Cancelar"
-          type="button"
-          className="text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200"
-          onClick={() => closeModal('addTaskModal')}
-        />
-        <Button
           text="Crear tarea"
           type="submit"
-          className="px-4 py-2 text-sm font-medium text-black bg-[#baff30] rounded-md hover:bg-[#baff30]/80 transition-colors"
+          className="px-4 py-2 text-sm font-medium text-black bg-[#baff30] rounded-md hover:bg-[#baff30]/80 transition-colors w-full"
         />
       </div>
     </form>
