21ba9615beac82d57198b3a420cc5df810da09ee
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-22 21:39:09 -0500

    fix: change edit button text

--- src/components/forms/edit-task-form.tsx
+++ src/components/forms/edit-task-form.tsx
@@ -46,7 +46,7 @@ export default function EditTaskForm() {
           }}
         />
         <Button
-          text="Crear tarea"
+          text="Guardar"
           type="submit"
           className="px-4 py-2 text-sm font-medium text-black bg-[#baff30] rounded-md hover:bg-[#baff30]/80 transition-colors"
         />
