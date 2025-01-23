fdc80559ef90776ff6587ae70ba8bac08ef63a27
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-22 21:01:29 -0500

    feat: add hooks for add and edit tasks

---
+++ src/hooks/use-add-task-form.tsx
@@ -0,0 +1,28 @@
+import useHandleTasks from './use-handle-tasks';
+import type { Task } from '@/types/task';
+import toast from 'react-hot-toast';
+import useHandleModal from './use-handle-modal';
+
+export default function useAddTaskForm() {
+  const { users, addTask } = useHandleTasks();
+  const { closeModal } = useHandleModal();
+
+  const submitAction = async (formData: FormData) => {
+    const data = Object.fromEntries(formData);
+
+    const { type, message } = await addTask(data as unknown as Task);
+
+    if (type === 'success') {
+      closeModal("addTaskModal");
+      toast.success(message);
+    } else {
+      toast.error(message);
+    }
+  };
+
+  return {
+    submitAction,
+    users,
+    closeModal,
+  };
+}
